8f8ef9bb8a1fb6784a0b9f91fd634ffcbaae1216
smoothwind <stephen.idle@qq.com>
2019-03-25 16:26:25 +0800

    更新config

--- src/gui/util/config.py
+++ src/gui/util/config.py
@@ -1,10 +1,11 @@
 # -*- coding: UTF-8 -*-
 import logging
 
-_LOG_FILE_NAME = 'default.log'
-_LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-_DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 LOG = logging
-LOG.basicConfig(filename=_LOG_FILE_NAME, level=logging.INFO, format=_LOG_FORMAT, datefmt=_DATE_FORMAT)
+
+file_op = open("default.log", encoding="utf-8", mode="a+")
+
+LOG.basicConfig(filename="default.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s",
+                datefmt="%m/%d/%Y %H:%M:%S %p")
 
 __ALL__ = ['LOG']
